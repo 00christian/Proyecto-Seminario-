db924e5a63b8dcf4ab931525880e068591ca884e
Christian Rodriguez <00christian.rodriguez@gmail.com>
2018-12-10 11:36:47 -0600

    integrando morgan, path, servidores fijos

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 const express = require ('express');
+const morgan = require ('morgan');
+const path = require('path');
 const app = express();
 
-app.listen(3000, () =>{
-    console.log('servidor en puerto 3000');
+//configuraciones 
+
+app.set('port',process.env.PORT || 3000);
+
+//middleware
+app.use(morgan('dev'));
+app.use(express.json());
+
+
+//router
+app.use('/api/task', require('./routes/task.routes'));
+
+//archivos staticos 
+//app.use
+app.use(express.static( path.join(__dirname, 'public')));
+
+//servidor 
+app.listen(app.get('port'), () => {
+    console.log(`Servidor en el puerto ${app.get('port')}`);
 
 });
